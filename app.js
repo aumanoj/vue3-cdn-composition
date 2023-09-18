768ddf5f358f6820ba5ac1c3e42d01e4e3e1c65b
Manoj Yadav <aumanojyadav@gmail.com>
2023-09-19 00:00:25 +0530

    created counter component refactor index file

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
 /* Include components here */
+import Counter from './components/Counter.js';
 import GreetingParent from './components/GreetingParent.js';
 import GreetingChild from './components/GreetingChild.js';
 import GreetingGrandChild from './components/GreetingGrandChild.js';
 /* Init app */
 const app = Vue.createApp({
     setup() {
-      const count = Vue.ref(0);
       const greetingMessage = 'Greeting from parrent app component';
-      const parentMessage = Vue.provide('parentMessage', greetingMessage);
-      return {
-        count,
-        parentMessage
-      };
+      Vue.provide('parentMessage', greetingMessage);;
     },
   });
   
   /* Register component here */
+  app.component('counter', Counter);
   app.component('greeting-parent', GreetingParent);
   app.component('greeting-child', GreetingChild);
   app.component('greeting-grand-child', GreetingGrandChild);
