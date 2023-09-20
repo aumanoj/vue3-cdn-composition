a9de62429da38ac56d1b9339490faa374d7aabb1
Manoj Yadav <aumanojyadav@gmail.com>
2023-09-20 21:23:48 +0530

    component called in app.js

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 /* Include components here */
-import Counter from './components/Counter.js';
-import GreetingParent from './components/GreetingParent.js';
-import GreetingChild from './components/GreetingChild.js';
-import GreetingGrandChild from './components/GreetingGrandChild.js';
+import Counter from './components/Counter.js'
+import GreetingParent from './components/GreetingParent.js'
+import GreetingChild from './components/GreetingChild.js'
+import GreetingGrandChild from './components/GreetingGrandChild.js'
 /* Init app */
 const app = Vue.createApp({
+    template: `
+      <!-- Counter sample -->
+      <counter></counter>
+      <!-- Parent component -->
+      <greeting-parent :message="greetingMessage"></greeting-parent>
+    `,
     setup() {
-      const greetingMessage = Vue.ref('Greeting from parrent app component');
-      Vue.provide('parentMessage', greetingMessage);;
+      const greetingMessage = Vue.ref('Greeting from parrent app component')
+      Vue.provide('parentMessage', greetingMessage)
     },
   });
   
   /* Register component here */
-  app.component('counter', Counter);
-  app.component('greeting-parent', GreetingParent);
-  app.component('greeting-child', GreetingChild);
-  app.component('greeting-grand-child', GreetingGrandChild);
+  app.component('counter', Counter)
+  app.component('greeting-parent', GreetingParent)
+  app.component('greeting-child', GreetingChild)
+  app.component('greeting-grand-child', GreetingGrandChild)
 
-  app.mount('#app');
+  app.mount('#app')
